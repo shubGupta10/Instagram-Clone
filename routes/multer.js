d25b7f35a562786d8600d0bf2d5f3e3c54a270de
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-24 17:11:06 +0530

    New Commit

--- routes/multer.js
+++ routes/multer.js
@@ -1,14 +1,15 @@
 const multer = require("multer");
-const {v4: uuidv4} = require('uuid');
 const path = require("path");
+const crypto = require("crypto");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/images/uploads");
   },
   filename: function (req, file, cb) {
-   const unique = uuidv4();
-    cb(null, unique + path.extname(file.originalname));
+    const fn =
+      crypto.randomBytes(16).toString("hex") + path.extname(file.originalname);
+    cb(null, fn);
   },
 });
 
